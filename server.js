f5ac66382a094af31afa1d27839634a475985e74
Matthew Lewis <matthew.tyler.lewis@gmail.com>
2023-09-27 11:16:42 -0700

    Add express

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require("http");
+const express = require('express');
 
-const routes = require("./routes")
+const app = express();
 
-const server = http.createServer(routes);
+app.use('/add-product', (req, res, next) =>{
+    console.log("One");
+    res.send('<h1>Add Product Pages</h1>');
+});
+
+app.use((req, res, next) =>{
+    console.log("Two");
+    res.send('<h1>Hello From Express!</h1>');
+})
+
+const server = http.createServer(app);
+
+
 
 server.listen(3000);
