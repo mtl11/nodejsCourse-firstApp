d1e7ee054121295716a0f11945fc5b122b29625f
Matthew Lewis <matthew.tyler.lewis@gmail.com>
2023-10-03 15:10:40 -0700

    module 5 additions

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const http = require("http");
 const express = require('express');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use('/add-product', (req, res, next) =>{
-    console.log("One");
-    res.send('<h1>Add Product Pages</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) =>{
-    console.log("Two");
-    res.send('<h1>Hello From Express!</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
-const server = http.createServer(app);
+app.listen(3000);
 
+// module.exports = path.dirname(require.main.filename);
 
-
-server.listen(3000);
